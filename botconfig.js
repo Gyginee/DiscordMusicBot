42638a4b8e65673188aafaf6b7a0138f3d1881e4
Nguyễn Minh Hùng <hungnguyenminh66@gmail.com>
2022-02-02 23:33:48 +0700

    commit

--- botconfig.js
+++ botconfig.js
@@ -1,18 +1,18 @@
 module.exports = {
-  Admins: ["317244850426740736"], //Admins of the bot
+  Admins: ["Id Admin"], //Admins of the bot
   ExpressServer: false, //If you wanted to make the website run or not
   DefaultPrefix: process.env.Prefix || "kd", //Default prefix, Server Admins can change the prefix
   Port: 3001, //Which port website gonna be hosted
-  SupportServer: "https://discord.gg/qdV2gVhmmg", //Support Server Link
-  Token: process.env.Token || "OTEyNzE4MjQ0MjExNzUyOTkx.YZ0BGg.qsGUklU0k0OnL_D2TtIuW8OfUxs", //Discord Bot Token
-  ClientID: process.env.Discord_ClientID || "912718244211752991", //Discord Client ID
-  ClientSecret: process.env.Discord_ClientSecret || "go4mN56JAgbJ46TW5XEiWS9kMpdkvqyN", //Discord Client Secret
+  SupportServer: "Support Server Link", //Support Server Link
+  Token: process.env.Token || "Discord Bot Token", //Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "Discord Client ID", //Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "Discord Client Secret", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
   ServerDeafen: true, //If you want bot to stay deafened
   DefaultVolume: 100, //Sets the default volume of the bot, You can change this number anywhere from 1 to 100
   CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
   "24/7": false, //If you want the bot to be stay in the vc 24/7
-  timeout: 1800000,
+  timeout: 1800000, //Timeout when queue end
   CookieSecret: "Gyginee is cute", //A Secret like a password
   IconURL:
     "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
@@ -29,7 +29,7 @@ module.exports = {
   //Lavalink
   Lavalink: {
     id: "14648",
-    host: "103.171.0.154",
+    host: "127.0.0.1",
     port: 2333, // The port that lavalink is listening to. This must be a number!
     pass: "youshallnotpass",
     secure: false, // Set this to true if the lavalink uses SSL or you're hosting lavalink on repl.it
@@ -37,7 +37,7 @@ module.exports = {
 
   //Please go to https://developer.spotify.com/dashboard/
   Spotify: {
-    ClientID: process.env.Spotify_ClientID || "ff598bdbb4864fbca35b6be2916c2590", //Spotify Client ID
-    ClientSecret: process.env.Spotify_ClientSecret || "77add901a2174342b3396f5b349c19a0", //Spotify Client Secret
+    ClientID: process.env.Spotify_ClientID || "Spotify Client ID", //Spotify Client ID
+    ClientSecret: process.env.Spotify_ClientSecret || "Spotify Client Secret", //Spotify Client Secret
   },
 };
